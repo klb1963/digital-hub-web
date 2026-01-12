1471dbf01b838201421bb3b32f343a604f60de36
Leonid Kleimann <k_l_b_1963@hotmail.com>
2026-01-12 16:27:46 +0100

    chore:imcrease logo - width 100

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export function Navbar() {
           <Image
             src="/logo-odh-512x256.png" // logo-odh-512x256.png logo-odh-512x256-inverted.png
             alt="Open Digital Hub logo"
-            width={120}
+            width={100}
             height={60}
             className="
             object-contain
